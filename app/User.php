9c399aef9efc0a32fff4283d3a0637aa39d104a6
Krystian Bondaruk <krystian.bondaruk@gmail.com>
2020-03-18 21:44:05 +0100

    Modify protected fillable - add new field - User

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+      'first_name', 'last_name', 'login', 'email', 'city', 'street', 'post_code', 'country_iso', 'password',
     ];
 
     /**
